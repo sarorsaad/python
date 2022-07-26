5c10f25a846eb816f4f2265f23e31cd613ac85f8
sarorsaad <sarorsaad@gmail.com>
2022-07-26 22:10:38 +0300

    variables and data type

---
+++ basic/variables.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+
+
+name='saror'
+age=40
+string='i live in suadi arabi'
+list=[1,2,4,8]
+tuple=(1,6,9)
+dic={1:'saror',2:40,}
+set={1,3,5}
+print(f'my name {name} my age is {age}  for 5 ys {string} i have list of {list} with tuple{tuple} and my dictinary is{dic} i collect it in a set {set}')
